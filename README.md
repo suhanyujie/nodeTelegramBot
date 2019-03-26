b705e5304770c63c43ccc2928fd408edbb3ef923
suhanyujie <suhanyujie@qq.com>
2019-03-26 13:32:30 +0800

    bot发送新闻到聊天室

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# nodeTelegramBot
+## nodeTelegramBot
+
+### install 
+* `npm run install`
+* `cp config/telegramBot.json.example config/telegramBot.json`
+* `node index.js`
+
+### 参考资料
+* `telegram bot`官方文档 https://core.telegram.org/bots/api#available-methods
+
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var cheerio = require('cheerio'),
-    http = require('http')
-
+    http = require('http'),
+    https = require('https'),
+    botConfig = require('./config/telegramBot')
 
 function get() {
     var options = {
@@ -28,14 +29,21 @@ get().then(function (res) {
         res.on('end',function(){  //数据传输完
             var data = Buffer.concat(chunks,size);
             var html = data.toString();
-            // console.log(html);
             newsList = parseHtml(html)
             // console.log(newsList);
             resolve(newsList)
         });
     })
 }).then((res)=>{
-    console.log(res);
+    return new Promise(function(resolve, reject){
+        resolve(renderHtml(res))
+    })
+}).then((res)=>{
+    return sendToTelegramBot(res)
+}).then((res)=>{
+    console.log(res)
+}).then((res)=>{
+    console.log(res)
 })
 
 function parseHtml(html) {
@@ -45,10 +53,65 @@ function parseHtml(html) {
         var oneNews = {};
         oneNews.link = $(this).find("a.latest_item").eq(0).attr('href');
         oneNews.title = $(this).find("a.latest_item").eq(0).find('p.latest_title').text();
-        console.log(oneNews);  //控制台输出
+        // console.log(oneNews);  //控制台输出
         newsList.push(oneNews);
     });
     return newsList
 }
 
+function renderHtml(newsList) {
+    if (newsList.length<1) return '';
+    let baseUrl = "http://www.flw.ph"
+    var html = "<strong>菲华网今日要闻：</strong>\n"
+    var contentArr = new Array()
+    newsList.forEach(function (value,key) {
+        value.link = baseUrl+"/"+value.link
+        contentArr.push('<b>'+(key+1)+'. </b><a href="'+value.link+'">'+value.title+'</a>'+"\n")
+    })
+    html += contentArr.join("")
+    return html
+}
 
+function sendToTelegramBot(content) {
+    let botConfigData = botConfig
+    console.log(botConfigData.apiToken)
+    var postData = {
+        chat_id:botConfigData.chatRoomId1,
+        parse_mode:"HTML",
+        text:content
+    }
+    content = JSON.stringify(postData);
+    var options = {
+        hostname: botConfigData.apiUrl,
+        port: 443,
+        path: '/bot'+botConfigData.apiToken+"/sendMessage",
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;'
+        }
+    };
+    return new Promise((resolve, reject)=>{
+        var resChunk = [],
+            size = 0
+        var req = https.request(options, function (res) {
+            console.log('STATUS: ' + res.statusCode);
+            console.log('HEADERS: ' + JSON.stringify(res.headers));
+            res.setEncoding('utf8');
+
+            res.on('data', function (chunk) {
+                console.log(chunk)
+                resChunk.push(chunk)
+                size += chunk.length
+            });
+            res.on('end',()=>{
+                console.log(resChunk)
+                resolve(resChunk)
+            })
+        });
+        req.on('error', function (e) {
+            console.log('problem with request: ' + e.message);
+        });
+        req.write(content)
+        req.end()
+    })
+}
